93da263ebbafbbe059fbf421b443a781146f495f
infrapale <infrapale@gmail.com>
2021-02-28 15:22:32 +0200

    New AIO library,  work in progress still

--- T205_Radio_Ada_IO.ino
+++ T205_Radio_Ada_IO.ino
@@ -7,9 +7,12 @@ T205 RFM69 Relay to Adafruit IO
 #include <SPI.h>
 #include <Wire.h>
 #include "config.h"
-#include <ESP8266WiFi.h>
-#include <Adafruit_MQTT.h>
-#include <Adafruit_MQTT_Client.h>
+#include "AdafruitIO_WiFi.h"
+#include <secrets.h>
+// #include <ESP8266WiFi.h>
+#include <Adafruit_Sensor.h>
+//#include <Adafruit_MQTT.h>
+//#include <Adafruit_MQTT_Client.h>
 #include <Adafruit_Sensor.h>
 #include "Adafruit_BME680.h"
 #include "ssd1306_feather.h"
@@ -17,7 +20,10 @@ T205 RFM69 Relay to Adafruit IO
 //#include "RTClib.h"
 //#include <SPI.h>
 //#include <SD.h>
-
+#define  VILLA_ASTRID 1
+//#define  LILLA_ASTRID 1
+//#define  SIIRTOLA 1
+//#include "secrets.h"
 
 /************ Radio Setup ***************/
 
@@ -56,6 +62,30 @@ T205 RFM69 Relay to Adafruit IO
   #define LED           13
 #endif
 
+#define  FLOAT_NO_VALUE 99999.0
+typedef struct DockSensors{
+    float water_temp;
+    float temperature;
+    float pressure;
+    float temp_dht22;
+    float hum_dht22;
+    float ldr1;
+    float ldr2;
+} DockSensor;
+
+typedef struct OD1_Sensors
+    float temperature;     
+    float temp_dht22;     
+    float hum_dht22;     
+    float ldr1;     
+    float ldr2;
+} OD1_Sensor;
+
+DockSensor   dock_sensor;
+OD1_Sensor   od1_sensor;
+
+
+AdafruitIO_WiFi io(IO_USERNAME, IO_KEY, WIFI_SSID, WIFI_PASS);
 // Singleton instance of the radio driver
 RH_RF69 rf69(RFM69_CS, RFM69_INT);
 Adafruit_BME680 bme; // I2C
@@ -74,37 +104,55 @@ boolean bme_ok = false;
 
 int16_t packetnum = 0;  // packet counter, we increment per xmission
 // Create an ESP8266 WiFiClient class to connect to the MQTT server.
-WiFiClient client;
+// WiFiClient client;
 // or... use WiFiFlientSecure for SSL
 //WiFiClientSecure client;
 
 // Setup the MQTT client class by passing in the WiFi client and MQTT server and login details.
+
 Adafruit_MQTT_Client mqtt(&client, AIO_SERVER, AIO_SERVERPORT, AIO_USERNAME, AIO_KEY);
 
 // Notice MQTT paths for AIO follow the form: <username>/feeds/<feedname>
 //Adafruit_MQTT_Subscribe onoffbutton = Adafruit_MQTT_Subscribe(&mqtt, AIO_USERNAME "/feeds/ONOFF_FEED");
-Adafruit_MQTT_Subscribe slider = Adafruit_MQTT_Subscribe(&mqtt, AIO_USERNAME "/feeds/villaastrid.ilmalampopumppu-temp");
+//Adafruit_MQTT_Subscribe slider = Adafruit_MQTT_Subscribe(&mqtt, AIO_USERNAME "/feeds/villaastrid.ilmalampopumppu-temp");
+AdafruitIO_Feed *slider = io.feed("villaastrid.ilmalampopumppu-temp");
 
-// Setup a feed called 'photocell' for publishing.
+
 // Notice MQTT paths for AIO follow the form: <username>/feeds/<feedname>
-Adafruit_MQTT_Publish water_temp = Adafruit_MQTT_Publish(&mqtt, AIO_USERNAME "/feeds/villaastrid.water-temp");
-Adafruit_MQTT_Publish dock_temp = Adafruit_MQTT_Publish(&mqtt, AIO_USERNAME "/feeds/villaastrid.dock-temp");
-Adafruit_MQTT_Publish dock_pres = Adafruit_MQTT_Publish(&mqtt, AIO_USERNAME "/feeds/villaastrid.dock-pres");
-Adafruit_MQTT_Publish dock_temp_dht22 = Adafruit_MQTT_Publish(&mqtt, AIO_USERNAME "/feeds/villaastrid.dock-temp-dht22");
-Adafruit_MQTT_Publish dock_hum_dht22 = Adafruit_MQTT_Publish(&mqtt, AIO_USERNAME "/feeds/villaastrid.dock-hum-dht22");
-Adafruit_MQTT_Publish dock_ldr1 = Adafruit_MQTT_Publish(&mqtt, AIO_USERNAME "/feeds/villaastrid.dock-ldr1");
-Adafruit_MQTT_Publish dock_ldr2 = Adafruit_MQTT_Publish(&mqtt, AIO_USERNAME "/feeds/villaastrid.dock-ldr2");
+//Adafruit_MQTT_Publish water_temp = Adafruit_MQTT_Publish(&mqtt, AIO_USERNAME "/feeds/villaastrid.water-temp");
+AdafruitIO_Feed *water_temp = io.feed("villaastrid.water-temp");
+//Adafruit_MQTT_Publish dock_temp = Adafruit_MQTT_Publish(&mqtt, AIO_USERNAME "/feeds/villaastrid.dock-temp");
+AdafruitIO_Feed *dock_temp = io.feed("villaastrid.dock-temp");
+//Adafruit_MQTT_Publish dock_pres = Adafruit_MQTT_Publish(&mqtt, AIO_USERNAME "/feeds/villaastrid.dock-pres");
+AdafruitIO_Feed *dock_pres = io.feed("villaastrid.dock-pres");
+//Adafruit_MQTT_Publish dock_temp_dht22 = Adafruit_MQTT_Publish(&mqtt, AIO_USERNAME "/feeds/villaastrid.dock-temp-dht22");
+AdafruitIO_Feed * = io.feed("villaastrid.dock-hum-dht22");
+//Adafruit_MQTT_Publish dock_hum_dht22 = Adafruit_MQTT_Publish(&mqtt, AIO_USERNAME "/feeds/villaastrid.dock-hum-dht22");
+AdafruitIO_Feed *dock_hum_dht22 = idock_temp_dht22o.feed("villaastrid.dock-hum-dht22");
+//Adafruit_MQTT_Publish dock_ldr1 = Adafruit_MQTT_Publish(&mqtt, AIO_USERNAME "/feeds/villaastrid.dock-ldr1");
+AdafruitIO_Feed *dock_ldr1 = io.feed("villaastrid.dock-ldr1");
+//Adafruit_MQTT_Publish dock_ldr2 = Adafruit_MQTT_Publish(&mqtt, AIO_USERNAME "/feeds/villaastrid.dock-ldr2");
+AdafruitIO_Feed *dock_ldr2 = io.feed("villaastrid.dock-ldr2");
 
-Adafruit_MQTT_Publish od1_temp = Adafruit_MQTT_Publish(&mqtt, AIO_USERNAME "/feeds/villaastrid.outdoor1-temp");
-Adafruit_MQTT_Publish od1_temp_dht22 = Adafruit_MQTT_Publish(&mqtt, AIO_USERNAME "/feeds/villaastrid.outdoor1-temp-dht22");
-Adafruit_MQTT_Publish od1_hum_dht22 = Adafruit_MQTT_Publish(&mqtt, AIO_USERNAME "/feeds/villaastrid.outdoor1-hum-dht22");
-Adafruit_MQTT_Publish od1_ldr1 = Adafruit_MQTT_Publish(&mqtt, AIO_USERNAME "/feeds/villaastrid.outdoor1-ldr1");
-Adafruit_MQTT_Publish od1_ldr2 = Adafruit_MQTT_Publish(&mqtt, AIO_USERNAME "/feeds/villaastrid.outdoor1-ldr2");
+//Adafruit_MQTT_Publish od1_temp = Adafruit_MQTT_Publish(&mqtt, AIO_USERNAME "/feeds/villaastrid.outdoor1-temp");
+AdafruitIO_Feed *od1_temp = io.feed("villaastrid.outdoor1-temp");
+//Adafruit_MQTT_Publish od1_temp_dht22 = Adafruit_MQTT_Publish(&mqtt, AIO_USERNAME "/feeds/villaastrid.outdoor1-temp-dht22");
+AdafruitIO_Feed *od1_temp_dht22 = io.feed("villaastrid.outdoor1-temp-dht22");
+//Adafruit_MQTT_Publish od1_hum_dht22 = Adafruit_MQTT_Publish(&mqtt, AIO_USERNAME "/feeds/villaastrid.outdoor1-hum-dht22");
+AdafruitIO_Feed *od1_hum_dht22 = io.feed("villaastrid.outdoor1-hum-dht22");
+//Adafruit_MQTT_Publish od1_ldr1 = Adafruit_MQTT_Publish(&mqtt, AIO_USERNAME "/feeds/villaastrid.outdoor1-ldr1");
+AdafruitIO_Feed *od1_ldr1 = io.feed("villaastrid.outdoor1-ldr1");
+//Adafruit_MQTT_Publish od1_ldr2 = Adafruit_MQTT_Publish(&mqtt, AIO_USERNAME "/feeds/villaastrid.outdoor1-ldr2");
+AdafruitIO_Feed *od1_ldr2 = io.feed("villaastrid.outdoor1-ldr2");
 
-Adafruit_MQTT_Publish tupa_temp = Adafruit_MQTT_Publish(&mqtt, AIO_USERNAME "/feeds/villaastrid.tupa-temp");
-Adafruit_MQTT_Publish tupa_hum = Adafruit_MQTT_Publish(&mqtt, AIO_USERNAME "/feeds/villaastrid.tupa-hum");
-Adafruit_MQTT_Publish tupa_pres = Adafruit_MQTT_Publish(&mqtt, AIO_USERNAME "/feeds/villaastrid.tupa-pres");
-Adafruit_MQTT_Publish tupa_gas = Adafruit_MQTT_Publish(&mqtt, AIO_USERNAME "/feeds/villaastrid.tupa-gas");
+//Adafruit_MQTT_Publish tupa_temp = Adafruit_MQTT_Publish(&mqtt, AIO_USERNAME "/feeds/villaastrid.tupa-temp");
+AdafruitIO_Feed *tupa_temp = io.feed("villaastrid.tupa-temp");
+//Adafruit_MQTT_Publish tupa_hum = Adafruit_MQTT_Publish(&mqtt, AIO_USERNAME "/feeds/villaastrid.tupa-hum");
+AdafruitIO_Feed *tupa_hum = io.feed("villaastrid.tupa-hum");
+//Adafruit_MQTT_Publish tupa_pres = Adafruit_MQTT_Publish(&mqtt, AIO_USERNAME "/feeds/villaastrid.tupa-pres");
+AdafruitIO_Feed *tupa_pres = io.feed("villaastrid.tupa-pres");
+//Adafruit_MQTT_Publish tupa_gas = Adafruit_MQTT_Publish(&mqtt, AIO_USERNAME "/feeds/villaastrid.tupa-gas");
+AdafruitIO_Feed *tupa_gas = io.feed("/feeds/villaastrid.tupa-gas");
 
 
 //Adafruit_MQTT_Publish water_temp = Adafruit_MQTT_Publish(&mqtt, AIO_USERNAME "/feeds/villaastrid.water-temp");
@@ -123,7 +171,7 @@ boolean sd_card_ok;
 String yyyy_mm_dd;
 //DateTime now;
 //File myFile;
-
+uint8_t  aio_state = 0;
 
 void setup() {
   delay(4000);
@@ -135,7 +183,8 @@ void setup() {
   // rtc.adjust(DateTime(F(__DATE__), F(__TIME__)));
 
   Serial.begin(115200);
-  //while (!Serial) { delay(1); } // wait until serial console is open, remove if not tethered to computer
+  delay(2000);
+  while (!Serial) { delay(1); } // wait until serial console is open, remove if not tethered to computer
   Serial.println("T205 RFM69 to Adafruit IO Relay");
   row[0] = "T2015 Radio-WiFi";
   
@@ -208,6 +257,17 @@ void setup() {
   //yyyy_mm_dd = String(now.year())+String(now.month())+String(now.day());
   //Serial.println(yyyy_mm_dd);
 
+    bme.setTemperatureOversampling(BME680_OS_8X);
+    bme.setHumidityOversampling(BME680_OS_2X);
+    bme.setPressureOversampling(BME680_OS_4X);
+    bme.setIIRFilterSize(BME680_FILTER_SIZE_3);
+    //bme.setGasHeater(320, 150); // 320*C for 150 ms
+    bme.setGasHeater(0, 0); // 320*C for 150 ms
+
+    memset(&dock_sensor,FLOAT_NO_VALUE,sizeof(dock_sensor));
+    memset(&od1_sensor,FLOAT_NO_VALUE,sizeof(od1_sensor));
+    
+
   poll_rfm_millis = millis();
   mqtt_connect_millis = millis();
   display_default_millis =millis();
@@ -221,12 +281,81 @@ void setup() {
 
 
 void loop(void) {
-   //now = rtc.now();
-   // Serial.print(bme_ok); Serial.println(sd_card_ok);
-   // ping the server to keep the mqtt connection alive
-   if(! mqtt.ping()) {
-     mqtt.disconnect();
-   }   
+
+   
+
+    io.run();
+    if (aio_state > 10) aio_state = 0;
+    switch (aio_state) {
+        case 0: 
+            bme.beginReading();
+            break;
+        case 1: 
+            if (!bme.endReading()) {
+                Serial.println("Failed to complete reading :(");
+                aio_state = 6;
+           } else {
+                aio_state++;
+           }
+        case 2: 
+            tupa_temp->save(bme.temperature);
+            aio_state++;
+            break; 
+        case 3: 
+            tupa_hum->save(bme.humidity);
+            aio_state++;
+            break; 
+        case 4: 
+            tupa_pres->save(bme.pressure);
+            aio_state++;
+            break; 
+        case 5: 
+            tupa_gas->save(bme.gas);
+            aio_state++;
+            break; 
+        case 6:
+            water_temp->save();
+            aio_state++;
+            break; 
+        case :
+            ->save();
+            aio_state++;
+            break; 
+        case :
+            ->save();
+            aio_state++;
+            break; 
+        case :
+            ->save();
+            aio_state++;
+            break; 
+        case :
+            ->save();
+            aio_state++;
+            break; 
+        case :
+            ->save();
+            aio_state++;
+            break; 
+        case :
+            ->save();
+            aio_state++;
+            break; 
+        case :
+            ->save();
+            aio_state++;
+            break; 
+        case :
+            ->save();
+            aio_state++;
+            break; 
+        case :
+            ->save();
+            aio_state++;
+            break; 
+
+   
+
    if ((millis() - mqtt_connect_millis ) > 1000 ){
       MQTT_connect();
       mqtt_connect_millis = millis();      
@@ -324,6 +453,7 @@ void RadioRxHandler(void){
    String sensor_name;
    String sensor_value;
    String inputString;
+   float  fsensor_value;
 
    if (rf69.available()) {
     // Should be a message for us now   
@@ -342,35 +472,28 @@ void RadioRxHandler(void){
       sensor_zone = parse_json(inputString,"{\"Z");
       sensor_name = parse_json(inputString,",\"S");
       sensor_value = parse_json(inputString,",\"V");
+      fsensor_value = sensor_value.toFloat();
       row[2] = sensor_zone; row[2].concat(":");
       row[2].concat(sensor_name);row[2].concat(":");
       row[2].concat(sensor_value);
       display_rows();
       Serial.println(sensor_zone); Serial.println(sensor_name); Serial.println(sensor_value);
       if (sensor_zone == "Dock"){
-        if (sensor_name == "T_Water") active_feed = &water_temp;
-        if (sensor_name == "T_bmp180") active_feed = &dock_temp;
-        if (sensor_name == "P_bmp180") active_feed = &dock_pres;
-        if (sensor_name == "T_dht22") active_feed = &dock_temp_dht22;
-        if (sensor_name == "H_dht22") active_feed = &dock_hum_dht22;
-        if (sensor_name == "ldr1") active_feed = &dock_ldr1;
-        if (sensor_name == "ldr2") active_feed = &dock_ldr2;
+        if (sensor_name == "T_Water")  dock_sensor.water_temp = fsensor_value;
+        if (sensor_name == "T_bmp180") dock_sensor.temperature = fsensor_value;
+        if (sensor_name == "P_bmp180") dock_sensor.pressure = fsensor_value;
+        if (sensor_name == "T_dht22")  dock_sensor.temp_dht22 = fsensor_value;
+        if (sensor_name == "H_dht22")  dock_sensor.hum_dht22 = fsensor_value;
+        if (sensor_name == "ldr1")     dock_sensor.ldr1 = fsensor_value;
+        if (sensor_name == "ldr2")     dock_sensor.ldr2 = fsensor_value;
       }
       if (sensor_zone == "OD_1"){
-         if (sensor_name == "Temp") active_feed = &od1_temp;     
-         if (sensor_name == "Temp2") active_feed = &od1_temp_dht22;     
-         if (sensor_name == "Hum") active_feed = &od1_hum_dht22;     
-         if (sensor_name == "Light1") active_feed = &od1_ldr1;     
-         if (sensor_name == "Light2") active_feed = &od1_ldr2;
+         if (sensor_name == "Temp")    od1_sensor.temperature = fsensor_value;     
+         if (sensor_name == "Temp2")   od1_sensor.temp_dht22 = fsensor_value;     
+         if (sensor_name == "Hum")     od1_sensor.hum_dht22 = fsensor_value;     
+         if (sensor_name == "Light1")  od1_sensor.ldr1 = fsensor_value;     
+         if (sensor_name == "Light2")  od1_sensor.ldr2 = fsensor_value;
       }      
-      if (! active_feed -> publish(sensor_value.toFloat())) {
-         Serial.println(F("Failed"));
-      } else {
-        Serial.println(F("OK!"));
-      }
-    } else {
-      Serial.println("Receive failed");
-    }
   }  
 }
 
